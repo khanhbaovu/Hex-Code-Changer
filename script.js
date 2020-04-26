b7fdb9844a27efeaae6b096ca39044567201854a
= <=>
2020-04-26 07:50:45 +0700

    2nd commit

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
-let codes = ["#6C1AE3", "#80E956", "#92BB53", "#DBC7FA", "#85E179", "#407902", "#FFF633", "#FF9633"];
-document.body.style.backgroundColor = "#D3D3D3";
+const button = document.querySelector("button");
+
+const body = document.querySelector("body");
+
+const codes = ["#6C1AE3", "#80E956", "#92BB53", "#DBC7FA", "#85E179", "#407902", "#FFF633", "#FF9633"];
+
+body.style.backgroundColor = "#D3D3D3";
+
+button.addEventListener(click, "codeChange");
 
 function codeChange() {
-  let ranNum = parseInt(Math.random()*codes.length);
+  const ranNum = parseInt(Math.random()*codes.length);
   document.getElementById('hexCode').innerHTML = codes[ranNum];
   document.body.style.backgroundColor = codes[ranNum];
 }
